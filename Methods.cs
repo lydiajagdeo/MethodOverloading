b67e4179c4093142241ba966f581087fc4b962e0
“Lydia <lydiajagdeo@gmail.com>
2022-08-16 12:29:14 -0400

    MethodOverloading initial

---
+++ Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace MethodOverloading
+{
+    public class Methods
+    {
+        public static int Add (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add (decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add( int num1, int num2, bool isMoney)
+        {
+            int sum = num1 + num2;
+            if (isMoney == true)
+            {
+                if (sum == 1)
+                {
+                    return $"{num1 + num2} dollar";
+                }
+                else if (sum >= 0)
+                {
+                    return $"{sum} dollars";
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Methods.Add(4, 6));
+            Console.WriteLine(Methods.Add(1.2m, 3.5m));
+            Console.WriteLine(Methods.Add(6, 4, true));
+        }
+    }
+}
+
